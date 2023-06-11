1b05298901319352446a06b44e9446793c520d1b
itsluismario <itsluismario@gmail.com>
2023-06-11 13:32:35 -0600

    :sparkles: Adding Navbar

--- itsluismario/src/components/Navbar/Navbar.tsx
+++ itsluismario/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className="flex flex-col  z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:from-inherit lg:static">
-        Hi,&nbsp;
-        <code className="font-mono font-bold">It's luismario</code>
-        </p>
+    <nav className="flex justify-between items-center z-10 w-full max-w-5xl text-sm lg:flex p-4 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:from-inherit lg:static">
+    <div className="text-white">
+      <Link legacyBehavior href="/">
+        <a className="text-xl font-bold">Hi, it's luismario</a>
+      </Link>
     </div>
+    <div className="space-x-4">
+      <Link legacyBehavior href="/about">
+        <a className="text-white hover:text-gray-400">About Me</a>
+      </Link>
+      <Link legacyBehavior href="/startup">
+        <a className="text-white hover:text-gray-400">Startups</a>
+      </Link>
+      <Link legacyBehavior href="/my-philosophy">
+        <a className="text-white hover:text-gray-400">My Philosophy</a>
+      </Link>
+    </div>
+  </nav>
   )
 }
 
