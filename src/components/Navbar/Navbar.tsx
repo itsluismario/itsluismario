2f4d03e7dac5a278fdbc372af8fd7d3aa4edf1ab
itsluismario <itsluismario@gmail.com>
2023-06-12 12:44:36 -0600

    :pencil2: Text has been substituted into the appropriate JSX elements

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import MobileMenu from '../MobileMenu/MobileMenu';
-import  { myInfoContext }  from '@src/context';
+import { myInfoContext } from '@src/context';
 
 const Navbar = () => {
   const router = useRouter();
@@ -26,11 +26,11 @@ const Navbar = () => {
     if (currentUrl !== '/') {
       return (
         <div className="space-x-4">
-        {context?.branchesList?.map((branch:any) => (
-          <Link legacyBehavior href={branch.url} key={branch.url}>
-            <a className="text-white hover:text-gray-400">{branch.title}</a>
-          </Link>
-        ))}
+          {context?.branchesList?.map((branch) => (
+            <Link legacyBehavior href={branch.url} key={branch.url}>
+              <a className="text-white hover:text-gray-400">{branch.title}</a>
+            </Link>
+          ))}
         </div>
       );
     }
@@ -40,10 +40,10 @@ const Navbar = () => {
     <nav className="flex justify-between items-center z-10 w-full max-w-5xl text-sm lg:flex p-4 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:from-inherit lg:static">
       <div className="text-white">
         <Link legacyBehavior href="/">
-          <a className="text-xl font-bold">Hi, it's luismario</a>
+          <a className="text-xl font-bold">Hi, it&apos;s luismario</a>
         </Link>
       </div>
-      {isMobile ? <MobileMenu/>: renderMenu()}
+      {isMobile ? <MobileMenu /> : renderMenu()}
     </nav>
   );
 };
--- src/components/Philosophy/index.tsx
+++ src/components/Philosophy/index.tsx
@@ -1,34 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 const PhilosophyContent = () => {
   return (
     <div className="text-left max-w-screen-sm group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30">
-
-        <h2 className="mb-3 text-2xl font-semibold">
+      <h2 className="mb-3 text-2xl font-semibold">
         How I see my life
-        </h2>
-          <h3 className="mb-3 text-xl font-semibold">
-          Peace is the key
-          </h3>
-          <p className="text-sm opacity-90 ">
-          As being part of Generation Z, after dedicating a reasonable amount of time to reflection, I have concluded that passion and enthusiasm are overrated. What I have discovered and found to be effective is the significance of identifying something that can be sustained in the long term. In this complex and ambiguous system, the key to maintaining stability lies in engaging in activities that bring inner peace.
-          </p>
-          <br/>
-          <h3 className="mb-3 text-xl font-semibold">
-          What the world will never stop needing  
-          </h3>
-          <p className="text-sm opacity-90 ">
-          Trying to be tolerant, actively listening, and avoiding being egocentric have helped me become a 1% better human being every day and transformed me into the person I would like to meet. 
-          </p>
-          <br/>
-          <h3 className="mb-3 text-xl font-semibold">
-          Collaborating as a team.
-          </h3>
-          <p className="text-sm opacity-90 ">
-          I have realized that nothing works in isolation; everything needs to be accomplished as a team. Therefore, I have adhered to three principles that have proven to be fantastic: full transparency and providing total access to information, prioritizing the best idea over position or role, and promptly resolving problems. Whenever I encounter a problem I can't solve, I make sure to reach out to someone else who can assist.
-          </p>
+      </h2>
+      <h3 className="mb-3 text-xl font-semibold">
+        Peace is the key
+      </h3>
+      <p className="text-sm opacity-90">
+        As being part of Generation Z, after dedicating a reasonable amount of time to reflection, I have concluded that passion and enthusiasm are overrated. What I have discovered and found to be effective is the significance of identifying something that can be sustained in the long term. In this complex and ambiguous system, the key to maintaining stability lies in engaging in activities that bring inner peace.
+      </p>
+      <br />
+      <h3 className="mb-3 text-xl font-semibold">
+        What the world will never stop needing
+      </h3>
+      <p className="text-sm opacity-90">
+        Trying to be tolerant, actively listening, and avoiding being egocentric have helped me become a 1% better human being every day and transformed me into the person I would like to meet.
+      </p>
+      <br />
+      <h3 className="mb-3 text-xl font-semibold">
+        Collaborating as a team.
+      </h3>
+      <p className="text-sm opacity-90">
+        I have realized that nothing works in isolation; everything needs to be accomplished as a team. Therefore, I have adhered to three principles that have proven to be fantastic: full transparency and providing total access to information, prioritizing the best idea over position or role, and promptly resolving problems. Whenever I encounter a problem I can&apos;t solve, I make sure to reach out to someone else who can assist.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default PhilosophyContent
+export default PhilosophyContent;
