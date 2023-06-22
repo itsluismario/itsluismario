261e363e79253d68ab8456d9874563a2ffad723e
itsluismario <itsluismario@gmail.com>
2023-06-21 20:29:22 -0600

    :sparkles: Adding Download CV

---
+++ src/components/CV/CV.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CV = () => {
+  return (
+    <div className="text-left max-w-screen-sm group rounded-lg ">
+    <button className="border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800 hover:bg-opacity-30 text-white font-bold rounded">
+      <a href='../../assets/CV_Luis_Mario_21_Jun_2023.pdf' download>Download CV</a>
+    </button>
+
+    </div>
+  )
+}
+
+export default CV
---
+++ src/pages/cv/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CV from '@src/components/CV/CV';
+import Skills from '@src/components/Skills/skills';
+import Navbar from '@src/components/Navbar/Navbar';
+
+const About = () => {
+  return (
+    <main className="flex flex-col items-center justify-between">
+        <CV/>
+    </main>
+  );
+};
+
+export default About;
