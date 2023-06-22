dd61ec0e5099d005d7868733c97ff0fc7566ba78
itsluismario <itsluismario@gmail.com>
2023-06-21 22:51:44 -0600

    :lipstick: Giving style to Open CV

--- src/pages/cv/index.tsx
+++ src/pages/cv/index.tsx
@@ -4,9 +4,11 @@ import Link from 'next/link';
 const CV = () => {
   return (
     <main className="flex flex-col items-center justify-between">
-      <Link href="https://sapphire-beatriz-43.tiiny.site" legacyBehavior>
-        <a>Open CV</a>
-      </Link>
+      <div className='text-left max-w-screen-sm group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800 hover:bg-opacity-30'>
+        <Link href="https://sapphire-beatriz-43.tiiny.site" legacyBehavior>
+          <a>Open CV</a>
+        </Link>
+      </div>
     </main>
   );
 };
