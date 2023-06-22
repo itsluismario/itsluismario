2a688a9705360bd1f48d55ecd277af678f0a6ae8
itsluismario <itsluismario@gmail.com>
2023-06-21 20:53:46 -0600

    :bug: Fixing bug to download cv

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const path = require('path');
+
 module.exports = {
-    webpack: (config) => {
-    config.module.rules.push({
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push(
+      {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-    });
+      },
+      {
+        test: /\.pdf$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      }
+    );
 
     return config;
-    },
-}
+  },
+};
--- src/components/CV/CV.tsx
+++ src/components/CV/CV.tsx
@@ -4,7 +4,9 @@ const CV = () => {
   return (
     <div className="text-left max-w-screen-sm group rounded-lg ">
     <button className="border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800 hover:bg-opacity-30 text-white font-bold rounded">
-      <a href='../../assets/CV_Luis_Mario_21_Jun_2023.pdf' download>Download CV</a>
+      <a href='../../assets/CV_Luis_Mario_21_Jun_2023.pdf' 
+        target="_blank"
+        download>Download CV</a>
     </button>
 
     </div>
