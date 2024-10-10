0e59dba71b5927e8a915e616b74b346fdba71839
itsluismario <itsluismario@gmail.com>
2024-10-10 16:38:43 -0600

    :memo: Adding more information to section My philosophy
    Adding more text about my hobbies

--- src/components/Philosophy/index.tsx
+++ src/components/Philosophy/index.tsx
@@ -3,39 +3,37 @@ import React from 'react';
 const PhilosophyContent = () => {
   return (
     <div className='text-left max-w-screen-sm group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800 hover:bg-opacity-30'>
-      <h2 className='mb-3 text-2xl font-semibold'>How I see my life</h2>
-      <h3 className='mb-3 text-xl font-semibold'>Consistency is the key</h3>
+      <h2 className='mb-3 text-2xl font-semibold'>My Philosophy (How I See My Life)</h2>
+      <h3 className='mb-3 text-md font-semibold'>Consistency Is the Key</h3>
       <p className='text-sm opacity-90 '>
-        I've learned that rewards are a byproduct of consistency. I realized this when <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>I lost 7.5 kilos of body fat.</label> Starting at 18% body fat, I'm now at 10%. I simply committed to 1.5 hours of daily exercise, cut out junk food, and drastically reduced alcohol.
+        I've learned that rewards are a byproduct of consistency. I realized this when <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>I lost 7.5 kilos of body fat.</label> Starting at 18% body fat, I'm now at 10%. I simply committed to 1.5 hours of daily exercise, cut out junk food, and drastically reduced my alcohol intake.
       </p>
       <br />
       <p className='text-sm opacity-90 '>
-       And ... I've reached a <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>700-day streak</label> on Duolingo.
+       Additionally, I've reached a <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>700-day streak</label> on Duolingo.
       </p>
       <br />
-      <h3 className='mb-3 text-xl font-semibold'>
-        Act by thinking in the sub-consequences of your actions
+      <h3 className='mb-3 text-md font-semibold'>
+        Act by Considering the Subconsequences of Your Actions
       </h3>
       <p className='text-sm opacity-90 '>
         When I lost the excess body fat, I never anticipated how great it would feel to be healthy. That's when my second lesson came.
       </p>
       <br />
-      <h3 className='mb-3 text-xl font-semibold'>Collaborating as a team.</h3>
+      <h3 className='mb-3 text-md font-semibold'>Collaborating as a Team</h3>
       <p className='text-sm opacity-90 '>
-        I have realized that nothing works in isolation; everything needs to be
-        accomplished as a team. Therefore, I have adhered to three principles
-        that have proven to be fantastic: full transparency and providing total
-        access to information, prioritizing the best idea over position or role,
-        and promptly resolving problems. Whenever I encounter a problem I
-        can&apos;t solve, I make sure to reach out to someone else who can
-        assist.
+        I have realized that nothing works in isolation; everything needs to be accomplished as a team. Therefore, I have adhered to three principles that have proven to be fantastic: full transparency and total access to information, prioritizing the best idea over position or role, and promptly resolving problems. Whenever I encounter a problem I can&apos;t solve, I make sure to reach out to someone else who can assist.
       </p>
       <br />
-      <h3 className='mb-3 text-xl font-semibold'>
-        Enjoy the process as much as you can
+      <h3 className='mb-3 text-md font-semibold'>
+        Enjoy the Process as Much as You Can
       </h3>
       <p className='text-sm opacity-90 '>
         As we grow up, we tend to forget to enjoy the process and focus solely on reaching the goal.
+      </p>
+      <br />
+      <p className='text-sm opacity-90 '>
+        I also spend my time having fun playing the guitar and running 10km <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>(my current record is 4:53/km).</label> 
       </p>
     </div>
   );
