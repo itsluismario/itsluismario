547ea3fb97a84bfe5a2be323ffe0b84dea53223a
itsluismario <itsluismario@gmail.com>
2024-10-10 16:17:46 -0600

    :memo: Updating the content at My Philosophy
    Adding:
    	- Consistency is the key
    	- Act by thinking in the sub-consequences of your actions
    	- Enjoy the process as much as you can

--- src/components/Philosophy/index.tsx
+++ src/components/Philosophy/index.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React from 'react';
 
 const PhilosophyContent = () => {
   return (
-    <div className="text-left max-w-screen-sm group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800 hover:bg-opacity-30">
-
-        <h2 className="mb-3 text-2xl font-semibold">
-        How I see my life
-        </h2>
-          <h3 className="mb-3 text-xl font-semibold">
-          Peace is the key
-          </h3>
-          <p className="text-sm opacity-90 ">
-          As being part of Generation Z, after dedicating a reasonable amount of time to reflection, I have concluded that passion and enthusiasm are overrated. What I have discovered and found to be effective is the significance of identifying something that can be sustained in the long term. In this complex and ambiguous system, the key to maintaining stability lies in engaging in activities that bring inner peace.
-          </p>
-          <br/>
-          <h3 className="mb-3 text-xl font-semibold">
-          What the world will never stop needing  
-          </h3>
-          <p className="text-sm opacity-90 ">
-          Trying to be tolerant, actively listening, and avoiding being egocentric have helped me become a 1% better human being every day and transformed me into the person I would like to meet. 
-          </p>
-          <br/>
-          <h3 className="mb-3 text-xl font-semibold">
-          Collaborating as a team.
-          </h3>
-          <p className="text-sm opacity-90 ">
-          I have realized that nothing works in isolation; everything needs to be accomplished as a team. Therefore, I have adhered to three principles that have proven to be fantastic: full transparency and providing total access to information, prioritizing the best idea over position or role, and promptly resolving problems. Whenever I encounter a problem I can&apos;t solve, I make sure to reach out to someone else who can assist.
-          </p>
+    <div className='text-left max-w-screen-sm group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800 hover:bg-opacity-30'>
+      <h2 className='mb-3 text-2xl font-semibold'>How I see my life</h2>
+      <h3 className='mb-3 text-xl font-semibold'>Consistency is the key</h3>
+      <p className='text-sm opacity-90 '>
+        I've learned that rewards are a byproduct of consistency. I realized this when <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>I lost 7.5 kilos of body fat.</label> Starting at 18% body fat, I'm now at 10%. I simply committed to 1.5 hours of daily exercise, cut out junk food, and drastically reduced alcohol.
+      </p>
+      <br />
+      <p className='text-sm opacity-90 '>
+       And ... I've reached a <label className='px-2 inline-flex rounded-full bg-purple-100 text-purple-800'>700-day streak</label> on Duolingo.
+      </p>
+      <br />
+      <h3 className='mb-3 text-xl font-semibold'>
+        Act by thinking in the sub-consequences of your actions
+      </h3>
+      <p className='text-sm opacity-90 '>
+        When I lost the excess body fat, I never anticipated how great it would feel to be healthy. That's when my second lesson came.
+      </p>
+      <br />
+      <h3 className='mb-3 text-xl font-semibold'>Collaborating as a team.</h3>
+      <p className='text-sm opacity-90 '>
+        I have realized that nothing works in isolation; everything needs to be
+        accomplished as a team. Therefore, I have adhered to three principles
+        that have proven to be fantastic: full transparency and providing total
+        access to information, prioritizing the best idea over position or role,
+        and promptly resolving problems. Whenever I encounter a problem I
+        can&apos;t solve, I make sure to reach out to someone else who can
+        assist.
+      </p>
+      <br />
+      <h3 className='mb-3 text-xl font-semibold'>
+        Enjoy the process as much as you can
+      </h3>
+      <p className='text-sm opacity-90 '>
+        As we grow up, we tend to forget to enjoy the process and focus solely on reaching the goal.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default PhilosophyContent
+export default PhilosophyContent;
